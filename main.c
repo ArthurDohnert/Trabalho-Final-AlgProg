99216a5a0575ae94bfb45e66042fe81b6f8fd53e
Arthur <arthurdohnert@gmail.com>
2024-07-08 20:58:44 -0300

    simular mapa, para depois botar os mapas de verdade

--- main.c
+++ main.c
@@ -7,36 +7,70 @@
 #define LARGURA 1200
 #define X_INICIAL 300
 #define Y_INICIAL 300
-#define VELOCIDADE 3
+#define VELOCIDADE 20
 #define TAMANHO_QUADRADO 20
 
-void movimentacao(int *pX, int *pY);
+void movimentacao(int *pX, int *pY, char mapa[60][30]);
 
 int main(void)
 {
     int posX = X_INICIAL, posY = Y_INICIAL;
-    int quadradoNaTela = 1;
+    int i, j;
     int *pX, *pY;
+    char mapa[60][30];
 
     pX = &posX;
     pY = &posY;
 
     InitWindow(LARGURA, ALTURA, "Infmon");
     SetTargetFPS(60);
-
     srand(time(NULL));
 
-    while (!WindowShouldClose() && quadradoNaTela)
+    for (i = 0; i < 60; i++)
     {
-        movimentacao(pX, pY);
+        for (j = 0; j < 30; j++)
+        {
+            if (i == 0 || i == 59 || j == 0 || j == 29)
+            {
+                mapa[i][j] = 'W';
+            }
+            else if (rand() % 5 == 1)
+            {
+                mapa[i][j] = 'G';
+            }
+            else
+            {
+                mapa[i][j] = ' ';
+            }
+        }
+    }
 
-        if (posX == 0 - TAMANHO_QUADRADO || posY == 0 - TAMANHO_QUADRADO || posX == LARGURA || posY == ALTURA)
-            quadradoNaTela = 0;
+    while (!WindowShouldClose())
+    {
+        movimentacao(pX, pY, mapa);
 
         BeginDrawing();
+        for (i = 0; i < 60; i++)
+        {
+            for (j = 0; j < 30; j++)
+            {
+                if (mapa[i][j] == 'G')
+                {
+                    DrawRectangle(i * 20, j * 20, 20, 20, DARKGREEN);
+                }
+                else if (mapa[i][j] == 'W')
+                {
+                    DrawRectangle(i * 20, j * 20, 20, 20, BLACK);
+                }
+                else if (mapa[i][j] == ' ')
+                {
+                    DrawRectangle(i * 20, j * 20, 20, 20, GREEN);
+                }
+            }
+        }
 
         ClearBackground(RAYWHITE);
-        DrawRectangle(posX, posY, TAMANHO_QUADRADO, TAMANHO_QUADRADO, GREEN);
+        DrawRectangle(posX, posY, TAMANHO_QUADRADO, TAMANHO_QUADRADO, RED);
         EndDrawing();
     }
 
@@ -44,14 +78,14 @@ int main(void)
     return 0;
 }
 
-void movimentacao(int *pX, int *pY)
+void movimentacao(int *pX, int *pY, char mapa[60][30])
 {
-    if (IsKeyDown(KEY_RIGHT))
+    if (IsKeyPressed(KEY_RIGHT) && mapa[(*pX + VELOCIDADE) / 20][*pY / 20] != 'W')
         *pX += VELOCIDADE;
-    if (IsKeyDown(KEY_LEFT))
+    if (IsKeyPressed(KEY_LEFT) && mapa[(*pX - VELOCIDADE) / 20][*pY / 20] != 'W')
         *pX -= VELOCIDADE;
-    if (IsKeyDown(KEY_UP))
+    if (IsKeyPressed(KEY_UP) && mapa[*pX / 20][(*pY - VELOCIDADE) / 20] != 'W')
         *pY -= VELOCIDADE;
-    if (IsKeyDown(KEY_DOWN))
+    if (IsKeyPressed(KEY_DOWN) && mapa[*pX / 20][(*pY + VELOCIDADE) / 20] != 'W')
         *pY += VELOCIDADE;
 }
